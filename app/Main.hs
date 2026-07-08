b5516a3a32f3d967609dc6be7b57d76fe8f258b9
Neil Hanak <neil.hanak@gmail.com>
2026-07-08 11:51:22 -0600

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -2,20 +2,30 @@ module Main (main) where
 
 import System.IO (hFlush, stdout)
 
+data EvaluatedResult = Print String | Exit
+
 main :: IO ()
 main = do
   putStr "$ "
   hFlush stdout
   command <- read'
-  print' $ eval command
-  hFlush stdout
-  main
+  handleEval $ eval command
 
 read' :: IO String
 read' = getLine
 
-eval :: String -> String
-eval command = command <> ": command not found"
+eval :: String -> EvaluatedResult
+eval command = case command of
+  "exit" -> Exit
+  otherwise -> Print $ command <> ": command not found"
 
-print' :: String -> IO ()
-print' = putStrLn
+handleEval :: EvaluatedResult -> IO ()
+handleEval evaluatedResult = case evaluatedResult of
+  Print str -> printAndContinue str
+  Exit -> pure ()
+
+printAndContinue :: String -> IO ()
+printAndContinue str = do
+  putStrLn str
+  hFlush stdout
+  main
