6f53c070b6099c93647705a8af49c665872a55ff
Neil Hanak <neil.hanak@gmail.com>
2026-07-08 11:20:01 -0600

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@ import System.IO (hFlush, stdout)
 
 main :: IO ()
 main = do
-    -- TODO: Uncomment the code below to pass the first stage
-    -- putStr "$ "
-    -- hFlush stdout
-    pure ()
+  -- TODO: Uncomment the code below to pass the first stage
+  putStr "$ "
+  hFlush stdout
+  pure ()
