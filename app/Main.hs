0ac2e79b19f54c93f644def21e05b49a3d436485
Neil Hanak <neil.hanak@gmail.com>
2026-09-04 12:17:39 -0600

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
-import Data.List (isInfixOf)
+import Data.Char (isSpace)
+import Data.List (dropWhileEnd, isInfixOf)
 import qualified Data.Text as T
 import Debug.Trace (traceShow)
 import System.Directory (doesDirectoryExist, findExecutable, getCurrentDirectory, getHomeDirectory, listDirectory, setCurrentDirectory)
@@ -29,7 +30,7 @@ read' :: IO String
 read' = getLine
 
 eval :: String -> IO EvaluatedResult
-eval args = if null args then pure Continue else eval' (getCommand args) (getRemainingArgs args)
+eval args = if null args then pure Continue else eval' (getCommand args) (getRemainingArgs' args)
 
 eval' :: String -> String -> IO EvaluatedResult
 eval' command args = case command of
@@ -48,9 +49,25 @@ eval' command args = case command of
         callProcess (takeFileName str) (words args)
         pure Continue
 
+-- args is one big string
+-- args can have values enclosed between ''
+-- treating args as a str is not going to work, to much random processing in random locations
+-- should treat args as [String]
+-- need to change getRemainingArgs
+
 replaceString :: String -> String -> String -> String
 replaceString old new haystack =
   T.unpack $ T.replace (T.pack old) (T.pack new) (T.pack haystack)
+
+getAllSubstrings :: Char -> Char -> String -> [String]
+getAllSubstrings _ _ [] = []
+getAllSubstrings start end str =
+  let initial = takeWhile (/= start) str
+   in [initial] ++ getAllSubstrings start end (safeTail (dropWhile (/= start) str))
+
+safeTail :: [a] -> [a]
+safeTail [] = []
+safeTail (x : xs) = xs
 
 handleChangeDirectoryCommand :: String -> IO EvaluatedResult
 handleChangeDirectoryCommand path = do
@@ -64,22 +81,30 @@ handleChangeDirectoryCommand path = do
       pure Continue
 
 handleTypeCommand :: String -> IO String
-handleTypeCommand remainingArgs = case remainingArgs of
+handleTypeCommand args = case args of
   x | x `elem` ["exit", "echo", "type", "pwd", "cd"] -> pure $ x <> " is a shell builtin"
-  _ -> _findExecutable remainingArgs
+  _ -> _findExecutable args
 
 _findExecutable :: String -> IO String
-_findExecutable remainingArgs = do
-  maybeFilePath <- findExecutable remainingArgs
+_findExecutable args = do
+  maybeFilePath <- findExecutable args
   case maybeFilePath of
     Just filePath -> pure filePath
-    Nothing -> pure (remainingArgs <> ": not found")
+    Nothing -> pure (args <> ": not found")
 
 getCommand :: String -> String
 getCommand args = head (words args)
 
-getRemainingArgs :: String -> String
-getRemainingArgs args = unwords (tail $ words args)
+-- getRemainingArgs :: String -> String
+-- getRemainingArgs args = unwords (tail $ words args)
+
+trim :: String -> String
+trim = dropWhileEnd isSpace . dropWhile isSpace
+
+getRemainingArgs' :: String -> String
+getRemainingArgs' args =
+  let (first, rest) = break (== ' ') args
+   in unwords $ filter (/= "") (getAllSubstrings '\'' '\'' (trim rest))
 
 handleEval :: EvaluatedResult -> IO ()
 handleEval evaluatedResult = case evaluatedResult of
