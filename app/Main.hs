3362dc0d77351c9a63ffa9af5f8b8227e6c1b54f
Neil Hanak <neil.hanak@gmail.com>
2026-09-10 09:34:48 -0600

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -22,6 +22,7 @@ main = do
   hFlush stdout
   args <- getLine
   evaluatedResult <- eval args
+  -- putStrLn ("[DEBUG]: evaluatedResult: " ++ (show evaluatedResult))
   handleEval evaluatedResult
 
 handleEval :: EvaluatedResult -> IO ()
