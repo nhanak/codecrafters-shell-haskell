0b9ef0af18160cb7b10fe3e35da5b6b767337636
Neil Hanak <neil.hanak@gmail.com>
2026-09-22 12:49:01 -0600

    codecrafters submit [skip ci]

--- app/Autocomplete/IO.hs
+++ app/Autocomplete/IO.hs
@@ -37,7 +37,7 @@ handleCompleterScriptNormalAutoCompletionState inputSoFar getInput' = do
     Nothing -> handleNoAutoCompleteFound inputSoFar getInput'
     Just completerScript -> do
       out <- io $ readProcess (path completerScript) [] ""
-      handleAutoCompleteFound (inputSoFar ++ (drop 1 out) ++ " ") getInput'
+      handleAutoCompleteFound (inputSoFar ++ (init out) ++ " ") getInput'
 
 handleFileNameNestedNormalAutoCompletionState :: String -> (String -> InputAutoCompletionState -> StateT ShellState IO String) -> StateT ShellState IO String
 handleFileNameNestedNormalAutoCompletionState inputSoFar getInput' = do
