f0c77afb974bee7111a58dbfd5bfb540f92c921f
Neil Hanak <neil.hanak@gmail.com>
2026-09-24 13:43:05 -0600

    codecrafters submit [skip ci]

--- app/Autocomplete/IO.hs
+++ app/Autocomplete/IO.hs
@@ -47,8 +47,11 @@ handleMultipleLineOutCompleterScript :: String -> String -> (String -> InputAuto
 handleMultipleLineOutCompleterScript inputSoFar out getInput' =
   let matches = filter (\x -> x /= "") (splitOn ['\n'] out)
    in case (findLongestCommonPrefix matches) of
-        Nothing -> traceShow ("[DEBUG]: NOTHING but matches are: " ++ show matches) (handleAutoCompleteMatchesFound inputSoFar matches getInput')
-        Just prefix -> traceShow ("[DEBUG]: JUST but mathces are: " ++ show matches) (handleAutoCompleteFound ((unwords (init (words inputSoFar))) ++ " " ++ prefix) getInput')
+        Nothing -> handleAutoCompleteMatchesFound inputSoFar matches getInput'
+        Just prefix -> handleAutoCompleteFound ((unwords (init (words inputSoFar))) ++ " " ++ prefix) getInput'
+
+-- Nothing -> traceShow ("[DEBUG]: NOTHING but matches are: " ++ show matches) (handleAutoCompleteMatchesFound inputSoFar matches getInput')
+-- Just prefix -> traceShow ("[DEBUG]: JUST but mathces are: " ++ show matches) (handleAutoCompleteFound ((unwords (init (words inputSoFar))) ++ " " ++ prefix) getInput')
 
 handleOneLineOutCompleterScript :: String -> String -> (String -> InputAutoCompletionState -> StateT ShellState IO String) -> StateT ShellState IO String
 handleOneLineOutCompleterScript inputSoFar out getInput' =
