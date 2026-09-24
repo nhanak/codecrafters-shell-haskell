7612f2c5c714f7ed42a6b1c993abd82424c6c585
Neil Hanak <neil.hanak@gmail.com>
2026-09-24 09:32:24 -0600

    codecrafters submit [skip ci]

--- app/Autocomplete/IO.hs
+++ app/Autocomplete/IO.hs
@@ -48,7 +48,7 @@ handleMultipleLineOutCompleterScript inputSoFar out getInput' =
   let matches = filter (\x -> x /= "") (splitOn ['\n'] out)
    in case (findLongestCommonPrefix matches) of
         Nothing -> handleAutoCompleteMatchesFound inputSoFar matches getInput'
-        Just prefix -> handleAutoCompleteFound ((unwords (init (words inputSoFar))) ++ " " ++ prefix) getInput'
+        Just prefix -> traceShow prefix (handleAutoCompleteFound ((unwords (init (words inputSoFar))) ++ " " ++ prefix) getInput')
 
 handleOneLineOutCompleterScript :: String -> String -> (String -> InputAutoCompletionState -> StateT ShellState IO String) -> StateT ShellState IO String
 handleOneLineOutCompleterScript inputSoFar out getInput' =
