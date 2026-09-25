6c6b5ce4b124e9ae1dccc85f6e981aa76a5793a5
Neil Hanak <neil.hanak@gmail.com>
2026-09-25 12:13:30 -0600

    codecrafters submit [skip ci]

--- app/ShellState/Core.hs
+++ app/ShellState/Core.hs
@@ -18,13 +18,25 @@ getCompleterScript' command_ completerScripts = case filter (\x -> command x == 
   _ -> Nothing
 
 formatBackgroundJobsForPrinting :: [BackgroundJob] -> [String]
-formatBackgroundJobsForPrinting backgroundJobs = if null backgroundJobs then [] else map (formatBackgroundJobForPrinting (backgroundJobPid (last backgroundJobs)) (length backgroundJobs)) backgroundJobs
+formatBackgroundJobsForPrinting backgroundJobs = if null backgroundJobs then [] else map (formatBackgroundJobForPrinting (getMostRecentBackgroundJobPid backgroundJobs) (getSecondMostRecentBackgroundJobPid backgroundJobs) (length backgroundJobs)) backgroundJobs
 
-formatBackgroundJobForPrinting :: Int -> Int -> BackgroundJob -> String
-formatBackgroundJobForPrinting mostRecentPid numJobs backgroundJob = idStr ++ marker ++ "  " ++ status ++ spacesAfterStatus ++ backgroundJobCommand backgroundJob ++ newLine
+formatBackgroundJobForPrinting :: Int -> Int -> Int -> BackgroundJob -> String
+formatBackgroundJobForPrinting mostRecentPid secondMostRecentPid numJobs backgroundJob = idStr ++ marker ++ "  " ++ status ++ spacesAfterStatus ++ backgroundJobCommand backgroundJob ++ newLine
   where
     idStr = "[" ++ show (backgroundJobId backgroundJob) ++ "]"
-    marker = if mostRecentPid == backgroundJobPid backgroundJob then "+" else ""
+    marker = getBackgroundJobMarker mostRecentPid secondMostRecentPid (backgroundJobPid backgroundJob)
     status = show $ backgroundJobStatus backgroundJob
     spacesAfterStatus = concat $ replicate (24 - length status) " "
     newLine = if numJobs > 1 then "\n" else ""
+
+getMostRecentBackgroundJobPid :: [BackgroundJob] -> Int
+getMostRecentBackgroundJobPid backgroundJobs = backgroundJobPid $ last backgroundJobs
+
+getSecondMostRecentBackgroundJobPid :: [BackgroundJob] -> Int
+getSecondMostRecentBackgroundJobPid backgroundJobs = if length backgroundJobs < 2 then (-1) else backgroundJobPid $ last $ init backgroundJobs
+
+getBackgroundJobMarker :: Int -> Int -> Int -> String
+getBackgroundJobMarker mostRecentPid secondMostRecentPid backgroundJobPid
+  | mostRecentPid == backgroundJobPid = "+"
+  | secondMostRecentPid == backgroundJobPid = "-"
+  | otherwise = " "
