fb081f9b6f60ccafbc1b7d3fdbc26de2dd10b1b1
Neil Hanak <neil.hanak@gmail.com>
2026-09-25 12:14:49 -0600

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -233,7 +233,7 @@ handleUnknownCommandBackground command args = do
 handleJobsCommand :: [String] -> StateT ShellState IO EvaluatedResult
 handleJobsCommand args = do
   backgroundJobs <- getBackgroundJobs
-  pure $ PrintStdOutAndContinue $ drop 1 $ concat $ formatBackgroundJobsForPrinting backgroundJobs
+  pure $ PrintStdOutAndContinue $ init $ concat $ formatBackgroundJobsForPrinting backgroundJobs
 
 handleCompleteCommand :: [String] -> StateT ShellState IO EvaluatedResult
 handleCompleteCommand args = case args of
