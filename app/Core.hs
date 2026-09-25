a88a1b3d246fa816c92ba1c867d8cfcf84ada373
Neil Hanak <neil.hanak@gmail.com>
2026-09-25 10:13:09 -0600

    codecrafters submit [skip ci]

--- app/Core.hs
+++ app/Core.hs
@@ -3,7 +3,7 @@ module Core (getArgsWithoutProcessPrioritySignifier, getProcessPriority, Process
 data ProcessPriority = Foreground | Background deriving (Show, Eq)
 
 getProcessPriority :: [String] -> ProcessPriority
-getProcessPriority args = if last args == "&" then Background else Foreground
+getProcessPriority args = if length args > 0 && last args == "&" then Background else Foreground
 
 getArgsWithoutProcessPrioritySignifier :: [String] -> [String]
 getArgsWithoutProcessPrioritySignifier args = if (getProcessPriority args) == Foreground then args else init args
